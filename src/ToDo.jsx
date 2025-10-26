547bf0b313d4d39beb6d9fcea646f991955c28e2
Aman <amantiwary161@gmail.com>
2025-10-26 12:49:58 +0530

    edit task fuctionalities added

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -5,6 +5,8 @@ const ToDo = () => {
         { task: 'go to gym', completed: false },
         { task: 'go to gym', completed: false },
     ])
+    const [editIndex, setEditIndex] = useState("")
+    const [editTask, setEditTask] = useState("")
     const addTask = () => {
         setTasks([...tasks, { task: task, completed: false }])
         setTask("")
@@ -20,6 +22,17 @@ const ToDo = () => {
         const newTasks = tasks.filter((ele) => ele.completed === false)
         setTasks(newTasks)
     }
+    const editingTaks = (index) => {
+        if (editIndex === index) {
+            tasks[editIndex].task = editTask
+            setEditTask("")
+            setEditIndex("")
+        }
+        else {
+            setEditTask(tasks[index].task)
+            setEditIndex(index)
+        }
+    }
     return (
         <div className='h-screen bg-black text-white pt-10'>
             <div className='bg-[#111] rounded-2xl p-5 max-w-[600px] m-auto flex flex-col gap-5'>
@@ -33,9 +46,8 @@ const ToDo = () => {
                 {/*Input and Butoon*/}
                 <div className="flex gap-2">
                     <input
-                        value={task}
+                        value={editTask}
                         onChange={(e) => { setTask(e.target.value); }}
-                        onKeyDown={(e) => { e.key === 'Enter' && addTask() }}
                         className='px-5 py-3 rounded-lg bg-[#222] outline-none w-full'
                         type='text' placeholder='Enter your task ....'></input>
                     <button onClick={addTask} className='px-5 py-3 bg-green-400 rounded-lg text-[#222]'>Add</button>
@@ -61,14 +73,29 @@ const ToDo = () => {
                 <div className='flex flex-col gap-3 h-[250px] overflow-y-scroll webkit-scrollbar '>
                     {
                         tasks.map((ele, index) => (
-                            <div className='flex justify-between bg-[#222] px-4 py-3 rounded-lg'>
-                                <div className='flex gap-2'>
-                                    <input className='accent-green-400' type='checkbox' id={index} />
-                                    <label for={index} className='text-xl cursor-pointer select-none'>{ele.task}</label>
-                                </div>
+                            <div className='flex justify-between bg-[#222] px-4 py-3 rounded-lg gap-5'>
+                                {
+                                    editIndex === index ? (
+                                        <input
+                                            autoFocus={true}
+                                            value={editTask}
+                                            onChange={(e) => setEditTask(e.target.value)}
+                                            onKeyDown={(e) => { e.key == 'Enter' && editingTaks(index) }}
+                                            className='w-full bg-transparent text-xl border-b-2 border-green-400 outline-none focus:border-green-300 transition-all' />
+
+                                    ) :
+                                        <div className='flex gap-2'>
+                                            <input className='accent-green-400' type='checkbox' id={index} />
+                                            <label for={index} className='text-xl cursor-pointer select-none'>{ele.task}</label>
+                                        </div>
+                                }
                                 <div className='flex gap-1'>
-                                    <span onClick={() => deleteTask(index)} className='h-5 w-5 bg-red-400 cursor-pointer rounded-full ' />
-                                    <span className='h-5 w-5 bg-yellow-400 cursor-pointer rounded-full ' />
+                                    <span
+                                        onClick={() => deleteTask(index)}
+                                        className='h-5 w-5 bg-red-400 cursor-pointer rounded-full ' />
+                                    <span
+                                        onClick={() => editingTaks(index)}
+                                        className={`h-5 w-5 cursor-pointer rounded-full ${editIndex === index ? 'bg-green-400' : 'bg-yellow-300'}`} />
                                 </div>
                             </div>
                         ))
